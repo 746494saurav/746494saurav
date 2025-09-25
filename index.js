fc559459b56789c33a04c31d212a10b382f44b35
Saurav Kumar <saurav7464skb@gmail.com>
2025-09-25 23:25:55 +0530

    add file

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import expres from 'express';
 const app=expres();
-
+app.use(expres.json());
+app.set('view engine','ejs');
 app.get('/',(req,resp)=>{
-    resp.send("Hello");
+    resp.render("index");   
 })
-
+app.post('/about',(req,resp)=>{
+    const {name, email, password}= req.body;
+    
+    resp.json(
+        {name, email, password}
+    )
+})
 app.listen(4000,(res,resp)=>{
     console.log("Server start on port :4000")
 })
